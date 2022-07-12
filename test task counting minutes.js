103b3114f3a03a1ebb28e2bfcff959eac7ad3fa0
ParkhomenkoDaniel <Parkhomenkodaniel@gmail.com>
2022-07-12 15:06:06 +0300

    Change code according to the review

--- test task counting minutes.js
+++ test task counting minutes.js
@@ -1,23 +1,18 @@
-<!DOCTYPE html>
-<script>
 
-function GetNum () {
-
-let min = prompt('Введите число?', 0);
-let hour = min / 60;
-let day = hour / 24;
-
-   if (Number(min) >= 1440) {
-     alert(Math.floor(day) + "d " 
-     + '0' + Math.floor(hour % 24) +  "h " 
-     + ('0' + min % 60).slice(-2) + "min");
-   } else if (Number(min) >= 60) {
-     alert('0' + Math.floor(hour % 24) +  "h " 
-     + ('0' + min % 60).slice(-2) + "min");
-   } else {
-     alert(('0' + min % 60).slice(-2) + "min");
-  }
-}
-GetNum();
-
-</script>
+function FormatDate(min) {
+  let hour = min / 60;
+  let day = hour / 24;
+  const MinsInTheDay = 1440;
+     if (Number(min) >= MinsInTheDay) {
+       return(Math.floor(day) + "d " 
+       + '0' + Math.floor(hour % 24) +  "h " 
+       + ('0' + min % 60).slice(-2) + "min");
+     } else if (Number(min) >= 60) {
+       return('0' + Math.floor(hour % 24) +  "h " 
+       + ('0' + min % 60).slice(-2) + "min");
+     } else {
+        return(('0' + min % 60).slice(-2) + "min");
+     }
+   }
+   
+   alert(FormatDate(234));
